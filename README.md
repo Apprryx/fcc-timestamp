1ce2c8595356e354364c9979c161251c8b7f108c
Apprryx <xtuxikx@gmail.com>
2017-05-07 01:03:51 +0200

    [del] some of the unnecesary files [finish] work on miniservice

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ Implementation of these user stories:
 
 **User Story:** If it does not contain a date or Unix timestamp, it returns null for those properties.
 
+This app is using moment.js to convert unix timestamp to natural date and the other way around. It's not perfect, but it does it's job :).
+
 ## Installation
 ```
 git clone https://github.com/Apprryx/fcc-timestamp
@@ -17,6 +19,12 @@ npm install
 npm start
 ```
 
+.env
+```
+SECRET=generate-your-secret
+PORT=8080
+APP_URL=http://localhost:8080/
+```
 ## License
 
 MIT License. [Click here for more information.](LICENSE.md)
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 'use strict';
-
 var express = require('express');
-var routes = require('./app/routes/index.js');
 var session = require('express-session');
-
+var path = process.cwd();
 var app = express();
+var moment = require('moment');
 require('dotenv').load();
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -14,7 +13,24 @@ app.use(session({
 	saveUninitialized: true
 }));
 
-routes(app);
+app.route('/')
+	.get(function (req, res) {
+		res.sendFile(path + '/public/index.html');
+  });
+app.route('/:timestamp')
+	.get(function (req, res) {
+		var obj = {natural: null, unix: null};
+		var timestamp = req.params.timestamp;
+		// this is not so great
+		if(moment.unix(timestamp).isValid() && timestamp >= 1000000) {
+			obj = {natural: moment.unix(timestamp).format('MMMM DD, YYYY'), unix: timestamp};
+		}
+		// this is also not great
+		else if (moment(timestamp).isValid() && timestamp.length > 8) {
+			obj = {natural: moment.utc(timestamp).format('MMMM DD, YYYY'), unix: moment.utc(timestamp).unix()};
+		}
+		res.json(obj);
+  });
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
